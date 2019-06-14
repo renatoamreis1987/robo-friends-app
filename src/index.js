f4dc0cd31ef518b8e2e5974adeccd44fbe2ba3ac
Renato Reis <email@renatoamreis.com>
2019-06-14 16:33:53 +0200

    Small change

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import thunkMiddleware from 'redux-thunk'
 import "./index.css";
 import App from "./containers/App";
 import * as serviceWorker from "./serviceWorker";
-import "tachyons";
+import 'tachyons';
 import { searchRobots, requestRobots } from "./reducers";
 
 
