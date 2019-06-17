5c7a207566ac50d78474640afaa59ccca1b46105
Renato Reis <email@renatoamreis.com>
2019-06-17 16:44:52 +0200

    Updates

---
+++ precache-manifest.515f342964db2ebde1af662c210bebe2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78dea135e7afa499881ace34de51d74e",
+    "url": "/robo-friends-app/index.html"
+  },
+  {
+    "revision": "42bfb8c9c93c5e0fe52f",
+    "url": "/robo-friends-app/static/css/2.e40df323.chunk.css"
+  },
+  {
+    "revision": "a93effe7e8d0ba45e67a",
+    "url": "/robo-friends-app/static/css/main.4b919b43.chunk.css"
+  },
+  {
+    "revision": "42bfb8c9c93c5e0fe52f",
+    "url": "/robo-friends-app/static/js/2.7e9d9553.chunk.js"
+  },
+  {
+    "revision": "a93effe7e8d0ba45e67a",
+    "url": "/robo-friends-app/static/js/main.ea1a190d.chunk.js"
+  },
+  {
+    "revision": "6ca832d65a9d754172c9",
+    "url": "/robo-friends-app/static/js/runtime~main.7e489c73.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends-app/static/media/SEGA.12a3db8b.woff"
+  }
+]);
