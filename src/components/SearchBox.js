9d5c2b016f4b166e7b66977532107343f6b813b4
Renato Reis <email@renatoamreis.com>
2019-06-18 10:37:45 +0200

    changes on SearchBox

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,6 +4,7 @@ const SearchBox = ({ SearchChange }) => {
     return (
         <div className='pa2'>
             <input 
+                label="search-robot"
                 className="pa3 ba b--green bg-lightest-blue"
                 type="search" 
                 placeholder="search robots" 
